7f4431ad1b40dd47ee3875446fce5d1fdc463393
Christoph Echtinger-Sieghart <sigi@0x2a.at>
2018-05-29 13:05:59 +0200

    Fix for "Academic License" version

--- exercise-3/run.sh
+++ exercise-3/run.sh
@@ -12,7 +12,7 @@ function ml() {
     local outfile=${OUTDIR}/${ml_function}.output
     # The sed part cuts the textual splash screen
     # The python part strips and cleans-up
-    ${MATLAB} ${MATLABFLAGS} -r "${ml_function};exit" ${ml_function}.m | sed -e "1,/${marker_line}/d" | python3 -c "import sys; print(sys.stdin.read().replace('\x1b\x5b\x3f\x31\x6c\x1b\x3e', '').strip())" > ${outfile}
+    ${MATLAB} ${MATLABFLAGS} -r "${ml_function};exit" ${ml_function}.m | sed -e "1,/${marker_line}/d" | python3 -c "import sys; print(sys.stdin.read().replace('\x1b\x5b\x3f\x31\x6c\x1b\x3e', '').replace('Academic License', '').strip())" > ${outfile}
 }
 
 for filename in team02*.m; do
